9f6577c46e7a32c26f33ba87180b198e9acc939a
“Foster <fosterataylor@yahoo.com>
2020-05-22 10:52:08 -0600

    Add project titles with larger links for slides

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -3,16 +3,18 @@ import Layout from "../components/layout";
 
 
 export default function Projects() {
+
+
   return (
     <Layout>
      <div class="slider">
   
-  <a href="#slide-1">1</a>
-  <a href="#slide-2">2</a>
-  <a href="#slide-3">3</a>
+  <a href="#slide-1">Market</a>
+  <a href="#slide-2">Soccer</a>
+  <a href="#slide-3">Movies</a>
 
   <div class="slides">
-        <div id="slide-1" >
+    <div id="slide-1" >
     </div>
         <div id="slide-2">
     </div>
