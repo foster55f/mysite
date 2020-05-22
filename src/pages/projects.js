5433b69355b0e98eb85c6fc6f03fca3955765669
“Foster <fosterataylor@yahoo.com>
2020-05-21 18:03:25 -0600

    Add new styling for portfolio structure

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -5,23 +5,21 @@ import Layout from "../components/layout";
 export default function Projects() {
   return (
     <Layout>
-      <div style={{display:'flex', flexDirection:'column', justifyContent:'space-between'}}>
-        <article>
-            <h1>Top Movies</h1>  
-            <div className='movie-project'></div>
-            <a href='https://limitless-bastion-36006.herokuapp.com/'>Top Movies site</a>   
-        </article> 
-        <article>
-            <h1>Euro Slider</h1>   
-            <div className='soccer-project'></div>   
-            <a href='https://morning-headland-35296.herokuapp.com/'>Euro Slider site</a>        
-        </article>
-        <article>
-            <h1 >Market2Table</h1> 
-            <div className='market-project'></div>   
-            <a href='https://market2table.herokuapp.com/'>Market2Table site</a>                     
-        </article>
-      </div>
+     <div class="slider">
+  
+  <a href="#slide-1">1</a>
+  <a href="#slide-2">2</a>
+  <a href="#slide-3">3</a>
+
+  <div class="slides">
+        <div id="slide-1" >
+    </div>
+        <div id="slide-2">
+    </div>
+        <div id="slide-3">
+    </div>
+  </div>
+</div>
     </Layout>
   )
 }
