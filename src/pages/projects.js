b8bb0385372243c14bd7a083d23f661014e97e62
“Foster <fosterataylor@yahoo.com>
2020-05-26 12:33:38 -0600

    Add production links for all projects

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -7,11 +7,11 @@ export default function Projects() {
 
   return (
     <Layout>
-        <div class="slider">
+        <div className="slider">
               <a href="#slide-1">Market2Table</a>              
             <a href="#slide-2">Top Movies</a>
             <a href="#slide-3">Euro Slider</a>
-            <div class="slides">
+            <div className="slides">
                 <div id="slide-1" >
                 </div>
                 <div id="slide-2">
@@ -20,7 +20,7 @@ export default function Projects() {
                 </div>
             </div>
           </div>
-          <div class="production-links">
+          <div className="production-links">
             <a href="https://market2table.herokuapp.com/">Market2Table Production Site</a>              
             <a href="https://limitless-bastion-36006.herokuapp.com/">Top Movies Production Site</a>
             <a href="https://morning-headland-35296.herokuapp.com/">Euro SliderProduction Site</a>
