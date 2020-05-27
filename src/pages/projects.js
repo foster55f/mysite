00f0d8f6d292219f498f80c54c85f7c8318adb28
“Foster <fosterataylor@yahoo.com>
2020-05-27 15:59:02 -0600

    Refactpr Projects

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,9 +1,7 @@
 import React from "react";
 import Layout from "../components/layout";
 
-
-export default function Projects() {
-
+const Projects = ()=> {
 
   return (
     <Layout>
@@ -28,3 +26,4 @@ export default function Projects() {
     </Layout>
   )
 }
+export default Projects
